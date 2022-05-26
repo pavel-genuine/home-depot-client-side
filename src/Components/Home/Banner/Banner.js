ba059d8896dd180abc44171c2d335ea91eb430f9
pavel.genuine <pavel.play@gmail.com>
2022-05-26 15:38:12 +0600

    portfolio

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,21 +1,42 @@
 import React from 'react';
 
 const Banner = () => {
-    return (
-        <div>
-            Banner
-            <div class="card card-side bg-base-100 shadow-xl">
-  <figure><img src="https://img.freepik.com/free-psd/mobile-phone-mockup-social-media_155435-297.jpg?w=1060" alt="Movie" /></figure>
-  <div class="card-body">
-    <h2 class="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div class="card-actions justify-end">
-      <button class="btn bg-[#53c5c5]">Watch</button>
-    </div>
-  </div>
-</div>
+  return (
+    <div>
+      <div className="carousel w-full">
+        <div id="item1" className="carousel-item w-full" >
+          <img src="https://api.lorem.space/image/car?w=800&h=200&hash=8B7BCDC2" className="w-full" />
         </div>
-    );
+        <div id="item2" className="carousel-item w-full">
+          <img src="https://api.lorem.space/image/car?w=800&h=200&hash=500B67FB" className="w-full" />
+        </div>
+        <div id="item3" className="carousel-item w-full">
+          <img src="https://api.lorem.space/image/car?w=800&h=200&hash=A89D0DE6" className="w-full" />
+        </div>
+        <div id="item4" className="carousel-item w-full">
+          <img src="https://api.lorem.space/image/car?w=800&h=200&hash=225E6693" className="w-full" />
+        </div>
+      </div>
+      
+     <div style={{width:'100%'}} className='flex justify-center items-center '>
+     <div style={{width:'100%'}} class=" flex justify-center items-center  card card-compact w-96 bg-base-100 shadow-xl ">
+        <div class="card-body">
+          <h2 class="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            {/* <button class="btn btn-primary">Buy Now</button> */}
+          </div>
+        </div>
+      </div> 
+       </div>     
+      <div className="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" className="btn btn-xs">1</a>
+        <a href="#item2" className="btn btn-xs">2</a>
+        <a href="#item3" className="btn btn-xs">3</a>
+        <a href="#item4" className="btn btn-xs">4</a>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -3,7 +3,42 @@ import React from 'react';
 const Portfolio = () => {
     return (
         <div>
-            Portfolio
+            <div>
+
+                <div style={{ padding: "50px", height: '60vh' }}>
+                    <h4 className='text-2xl'>My Self</h4>
+                    <p>I'm Joynul Abedin Pavel, a CSE student from NSU. I want to start a tech based start-up soon which will work to solve some burning problems of our country.
+                        It's now at a brainstorming stage. One of BUET students,  my co-partner and I are planning to work with supply chain related problems to solve and make smooth using technology.
+                        I supposed to work for skeleton part especially website development and my partner will work to integrate the system with deep learning technology.
+                        Now we all are building and sharpening our skills to build something interesting.
+
+
+                        We're very passionate and dedicated about this project and hoping a remarkable achievement. </p>
+                    <br /> 
+                    <p>
+                        I have some decent skills on web development and problem solvings.
+                    </p>
+                    <br />
+                    <div>
+                        <p>
+                            Some of my works :
+
+                        </p> <br />
+                        <button className='btn'><p><a href="https://full-stack-11.web.app/"> link 1</a></p>
+                        </button>      <br />
+                        <button className='btn my-5'>
+                            <p><a href="https://fir-red-onion.web.app/"> link 2</a></p>
+                        </button> <br />
+                        <button className='btn'>
+                            <p><a href="https://assignment-9-routes.netlify.app/"> link 3</a></p>
+
+                        </button>
+
+                    </div>
+                </div>
+
+
+            </div>
         </div>
     );
 };
